704a14e5f0472f0ac54316fe2cd88610708b2cce
luoxn28 <luoxn28@163.com>
2018-08-14 10:29:06 +0800

    empty boot-demo

--- src/main/java/com/luo/demo/boot/BootApplication.java
+++ src/main/java/com/luo/demo/boot/BootApplication.java
@@ -1,17 +1,13 @@
 package com.luo.demo.boot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author xiangnan
  * date 2018/7/27 17:52
  */
 @SpringBootApplication
-@MapperScan("com.luo.demo.boot.dao")
-@ServletComponentScan
 public class BootApplication {
 
     public static void main(String[] args) {
